aed75c2ecce3355831dcedc7a5abe0ca786cfdcb
surtandri <56786703+surtandri@users.noreply.github.com>
2021-02-12 09:50:26 +0800

    Add files via upload

---
+++ db_cmp.sql
@@ -0,0 +1,249 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 12, 2021 at 02:03 AM
+-- Server version: 10.4.6-MariaDB
+-- PHP Version: 7.3.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_cmp`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `barang`
+--
+
+CREATE TABLE `barang` (
+  `id_barang` int(11) NOT NULL,
+  `kode_barang` varchar(191) NOT NULL,
+  `id_kategori` int(11) NOT NULL,
+  `id_merk` int(11) NOT NULL,
+  `id_distributor` int(11) NOT NULL,
+  `nama_kendaraan` varchar(191) NOT NULL,
+  `nama_barang` varchar(191) NOT NULL,
+  `harga_beli` int(11) NOT NULL,
+  `harga_jual1` int(11) NOT NULL,
+  `harga_jual2` int(11) NOT NULL,
+  `satuan_barang` varchar(191) NOT NULL,
+  `stok` int(11) NOT NULL,
+  `tgl_input` varchar(191) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `barang`
+--
+
+INSERT INTO `barang` (`id_barang`, `kode_barang`, `id_kategori`, `id_merk`, `id_distributor`, `nama_kendaraan`, `nama_barang`, `harga_beli`, `harga_jual1`, `harga_jual2`, `satuan_barang`, `stok`, `tgl_input`) VALUES
+(2, 'MAXVCT', 2, 24, 2, 'Universal', 'Victra 140/70-14 TL', 300000, 400000, 390000, 'PCS', 20, '11-02-2021, (16:48:33)'),
+(5, 'a', 2, 24, 2, 'a', 'a', 1, 1, 1, 'PCS', 20, '11-02-2021, (15:54:38)');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `distributor`
+--
+
+CREATE TABLE `distributor` (
+  `id_distributor` int(11) NOT NULL,
+  `nama_distributor` varchar(191) NOT NULL,
+  `sales` varchar(191) NOT NULL,
+  `hp` varchar(191) NOT NULL,
+  `alamat_distributor` varchar(191) NOT NULL,
+  `tgl_input` varchar(191) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `distributor`
+--
+
+INSERT INTO `distributor` (`id_distributor`, `nama_distributor`, `sales`, `hp`, `alamat_distributor`, `tgl_input`) VALUES
+(2, 'PT.CENTRAL MOTOR DANESWARA', 'Rifani', '081818181818', 'Jalan hidayatullah', '11-02-2021, (16:51:57)');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kategori`
+--
+
+CREATE TABLE `kategori` (
+  `id_kategori` int(11) NOT NULL,
+  `nama_kategori` varchar(191) NOT NULL,
+  `tgl_input` varchar(191) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `kategori`
+--
+
+INSERT INTO `kategori` (`id_kategori`, `nama_kategori`, `tgl_input`) VALUES
+(2, 'Ban Luar', '09-02-2021, (12:45:06)'),
+(3, 'Ban Dalam', '09-02-2021, (12:33:25)');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `member`
+--
+
+CREATE TABLE `member` (
+  `id_member` int(11) NOT NULL,
+  `nama_member` varchar(191) NOT NULL,
+  `alamat_member` text NOT NULL,
+  `telepon` varchar(191) NOT NULL,
+  `email` varchar(191) NOT NULL,
+  `gambar` varchar(191) NOT NULL,
+  `NIK` varchar(191) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `member`
+--
+
+INSERT INTO `member` (`id_member`, `nama_member`, `alamat_member`, `telepon`, `email`, `gambar`, `NIK`) VALUES
+(1, 'Suryanto Tandriz', 'jl.gunung bromo', '081212345678', 'suryanto@gmail.com', 'tandri.jpg', '214748364732');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `merk`
+--
+
+CREATE TABLE `merk` (
+  `id_merk` int(11) NOT NULL,
+  `nama_merk` varchar(191) NOT NULL,
+  `tgl_input` varchar(191) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `merk`
+--
+
+INSERT INTO `merk` (`id_merk`, `nama_merk`, `tgl_input`) VALUES
+(23, 'Maxxis', '09-02-2021, (12:43:55)'),
+(24, 'Michellin', '04-02-2021, (20:57:27)'),
+(25, 'FDR', '04-02-2021, (20:57:30)'),
+(26, 'Kingsland', '04-02-2021, (20:57:34)'),
+(27, 'Swallow', '04-02-2021, (20:57:37)'),
+(34, 'IRC', '05-02-2021, (10:16:51)'),
+(35, 'Bridgestone', '05-02-2021, (10:16:59)');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `username` varchar(191) NOT NULL,
+  `password` varchar(191) NOT NULL,
+  `id_member` int(11) NOT NULL,
+  `role` varchar(191) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `password`, `id_member`, `role`) VALUES
+(1, 'admin', '$2y$10$jf0/JAUMWXROA3LCrxIreueB8DGlx/WVZb/z1T3g0KGrvgE/urd/S', 1, 'admin');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `barang`
+--
+ALTER TABLE `barang`
+  ADD PRIMARY KEY (`id_barang`);
+
+--
+-- Indexes for table `distributor`
+--
+ALTER TABLE `distributor`
+  ADD PRIMARY KEY (`id_distributor`);
+
+--
+-- Indexes for table `kategori`
+--
+ALTER TABLE `kategori`
+  ADD PRIMARY KEY (`id_kategori`);
+
+--
+-- Indexes for table `member`
+--
+ALTER TABLE `member`
+  ADD PRIMARY KEY (`id_member`);
+
+--
+-- Indexes for table `merk`
+--
+ALTER TABLE `merk`
+  ADD PRIMARY KEY (`id_merk`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `barang`
+--
+ALTER TABLE `barang`
+  MODIFY `id_barang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `distributor`
+--
+ALTER TABLE `distributor`
+  MODIFY `id_distributor` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `kategori`
+--
+ALTER TABLE `kategori`
+  MODIFY `id_kategori` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `member`
+--
+ALTER TABLE `member`
+  MODIFY `id_member` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `merk`
+--
+ALTER TABLE `merk`
+  MODIFY `id_merk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
